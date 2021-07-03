e2b7bc03a376f8f2a68c2b111bb5cd70ba110504
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-07-04 01:13:16 +0200

    rename cinder flour ore piece to dust

--- src/main/java/com/kotakotik/createautomated/register/RecipeItems.java
+++ src/main/java/com/kotakotik/createautomated/register/RecipeItems.java
@@ -7,6 +7,8 @@ import com.kotakotik.createautomated.register.recipes.ModMixingRecipes;
 import com.simibubi.create.AllItems;
 import com.simibubi.create.content.contraptions.processing.HeatCondition;
 import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.simibubi.create.repack.registrate.builders.BlockBuilder;
+import com.simibubi.create.repack.registrate.builders.ItemBuilder;
 import com.simibubi.create.repack.registrate.providers.RegistrateRecipeProvider;
 import com.simibubi.create.repack.registrate.util.DataIngredient;
 import com.simibubi.create.repack.registrate.util.entry.BlockEntry;
@@ -39,13 +41,14 @@ public class RecipeItems {
 
         List<BiConsumer<RegistrateRecipeProvider, ExtractableResource>> recipeGen = new ArrayList<>();
         public WorldGen.FeatureToRegister oreGenFeature;
+        public Function<BlockBuilder<NodeBlock, CreateRegistrate>, BlockBuilder<NodeBlock, CreateRegistrate>> nodeConf = c -> c;
 
-        public ExtractableResource(String name, CreateRegistrate reg) {
+        public ExtractableResource(String name, CreateRegistrate reg, Function<ItemBuilder<Item, CreateRegistrate>, ItemBuilder<Item, CreateRegistrate>> orePieceConf) {
             this.name = name;
             this.reg = reg;
 
-            ORE_PIECE = reg.item(name + "_ore_piece", Item::new).recipe((ctx, prov) -> recipeGen.forEach(r -> r.accept(prov, this))).tag(ModTags.Items.ORE_PIECES).model(($, $$) -> {
-            }).register();
+            ORE_PIECE = orePieceConf.apply(reg.item(name + "_ore_piece", Item::new).recipe((ctx, prov) -> recipeGen.forEach(r -> r.accept(prov, this))).tag(ModTags.Items.ORE_PIECES).model(($, $$) -> {
+            })).register();
         }
 
         public ExtractableResource oreGen(int veinSize, int minHeight, int maxHeight, int frequency, WorldGen.NodeDimension dim) {
@@ -60,15 +63,15 @@ public class RecipeItems {
             return oreGen(veinSize, 40, 256, frequency, dimension);
         }
 
-        public ExtractableResource node(int minOre, int maxOre, Function<TopOreExtractorBlock.ExtractorProgressBuilder, Integer> progress) {
-            NODE = reg.block(name + "_node", p -> new NodeBlock(p, ORE_PIECE, maxOre, minOre, progress.apply(new TopOreExtractorBlock.ExtractorProgressBuilder()))).blockstate(($, $$) -> {
+        public ExtractableResource node(int minOre, int maxOre, Function<TopOreExtractorBlock.ExtractorProgressBuilder, Integer> progress, Function<BlockBuilder<NodeBlock, CreateRegistrate>, BlockBuilder<NodeBlock, CreateRegistrate>> conf) {
+            NODE = conf.apply(reg.block(name + "_node", p -> new NodeBlock(p, ORE_PIECE, maxOre, minOre, progress.apply(new TopOreExtractorBlock.ExtractorProgressBuilder()))).blockstate(($, $$) -> {
             }).tag(ModTags.Blocks.NODES).item().model(($, $$) -> {
-            }).build().register();
+            }).build()).register();
             return this;
         }
 
-        public ExtractableResource node(int ore, Function<TopOreExtractorBlock.ExtractorProgressBuilder, Integer> progress) {
-            return node(ore, ore, progress);
+        public ExtractableResource node(int ore, Function<TopOreExtractorBlock.ExtractorProgressBuilder, Integer> progress, Function<BlockBuilder<NodeBlock, CreateRegistrate>, BlockBuilder<NodeBlock, CreateRegistrate>> conf) {
+            return node(ore, ore, progress, conf);
         }
 
         public ExtractableResource recipe(BiConsumer<RegistrateRecipeProvider, ExtractableResource> consumer) {
@@ -81,14 +84,14 @@ public class RecipeItems {
         public final boolean autoCreateRecipes;
         public ItemEntry<Item> INGOT_PIECE;
 
-        public IngotExtractableResource(String name, CreateRegistrate reg, boolean autoCreateRecipes, @Nullable NonNullSupplier<Item> ingot) {
-            super(name, reg);
+        public IngotExtractableResource(String name, CreateRegistrate reg, boolean autoCreateRecipes, @Nullable NonNullSupplier<Item> ingot, Function<ItemBuilder<Item, CreateRegistrate>, ItemBuilder<Item, CreateRegistrate>> orePieceConf, Function<ItemBuilder<Item, CreateRegistrate>, ItemBuilder<Item, CreateRegistrate>> ingotPieceConf) {
+            super(name, reg, orePieceConf);
             this.autoCreateRecipes = autoCreateRecipes;
 
-            INGOT_PIECE = reg.item(name + "_ingot_piece", Item::new)
+            INGOT_PIECE = ingotPieceConf.apply(reg.item(name + "_ingot_piece", Item::new)
                     .tag(ModTags.Items.INGOT_PIECES)
                     .model(($, $$) -> {
-                    }).register();
+                    })).register();
 
             if (autoCreateRecipes) {
                 recipe((provider, $) -> {
@@ -105,8 +108,8 @@ public class RecipeItems {
     }
 
     public static class GlueableExtractableResource extends ExtractableResource {
-        public GlueableExtractableResource(String name, CreateRegistrate reg, boolean autoCreateRecipes, NonNullSupplier<Item> output) {
-            super(name, reg);
+        public GlueableExtractableResource(String name, CreateRegistrate reg, boolean autoCreateRecipes, NonNullSupplier<Item> output, Function<ItemBuilder<Item, CreateRegistrate>, ItemBuilder<Item, CreateRegistrate>> orePieceConf) {
+            super(name, reg, orePieceConf);
 
             if (autoCreateRecipes) {
                 recipe((provider, $) -> MIXING.add(name + "_gluing", b -> {
@@ -127,28 +130,28 @@ public class RecipeItems {
     public static ExtractableResource CINDER_FLOUR_EXTRACTABLE;
 
     public static void register(CreateRegistrate registrate) {
-        LAPIS_EXTRACTABLE = new GlueableExtractableResource("lapis", registrate, true, () -> Items.LAPIS_LAZULI)
-                .node(1, 4, (b) -> b.atSpeedOf(128).takesSeconds(30).build())
+        LAPIS_EXTRACTABLE = new GlueableExtractableResource("lapis", registrate, true, () -> Items.LAPIS_LAZULI, c -> c)
+                .node(1, 4, (b) -> b.atSpeedOf(128).takesSeconds(30).build(), c -> c)
                 .oreGen(10, 4, WorldGen.NodeDimension.OVERWORLD);
 
-        IRON_EXTRACTABLE = new IngotExtractableResource("iron", registrate, true, () -> Items.IRON_INGOT)
-                .node(0, 2, (b) -> b.atSpeedOf(128).takesMinutes(3).build())
+        IRON_EXTRACTABLE = new IngotExtractableResource("iron", registrate, true, () -> Items.IRON_INGOT, c -> c, c -> c)
+                .node(0, 2, (b) -> b.atSpeedOf(128).takesMinutes(3).build(), c -> c)
                 .oreGen(4, 1, WorldGen.NodeDimension.OVERWORLD);
 
-        ZINC_EXTRACTABLE = new IngotExtractableResource("zinc", registrate, true, AllItems.ZINC_INGOT)
-                .node(1, 2, (b) -> b.atSpeedOf(128).takesSeconds(40).build())
+        ZINC_EXTRACTABLE = new IngotExtractableResource("zinc", registrate, true, AllItems.ZINC_INGOT, c -> c, c -> c)
+                .node(1, 2, (b) -> b.atSpeedOf(128).takesSeconds(40).build(), c -> c)
                 .oreGen(9, 2, WorldGen.NodeDimension.OVERWORLD);
 
-        GOLD_EXTRACTABLE = new IngotExtractableResource("gold", registrate, true, () -> Items.GOLD_INGOT)
-                .node(0, 2, (b) -> b.atSpeedOf(128).takesMinutes(2).build())
+        GOLD_EXTRACTABLE = new IngotExtractableResource("gold", registrate, true, () -> Items.GOLD_INGOT, c -> c, c -> c)
+                .node(0, 2, (b) -> b.atSpeedOf(128).takesMinutes(2).build(), c -> c)
                 .oreGen(6, 1, WorldGen.NodeDimension.OVERWORLD);
 
-        COPPER_EXTRACTABLE = new IngotExtractableResource("copper", registrate, true, AllItems.COPPER_INGOT)
-                .node(1, 4, (b) -> b.atSpeedOf(128).takesSeconds(40).build())
+        COPPER_EXTRACTABLE = new IngotExtractableResource("copper", registrate, true, AllItems.COPPER_INGOT, c -> c, c -> c)
+                .node(1, 4, (b) -> b.atSpeedOf(128).takesSeconds(40).build(), c -> c)
                 .oreGen(16, 2, WorldGen.NodeDimension.OVERWORLD);
 
-        CINDER_FLOUR_EXTRACTABLE = new ExtractableResource("cinder_flour", registrate)
-                .node(1, 3, b -> b.atSpeedOf(128).takesSeconds(15).build())
+        CINDER_FLOUR_EXTRACTABLE = new ExtractableResource("cinder_flour", registrate, c -> c.lang("Cinder Flour Dust"))
+                .node(1, 3, b -> b.atSpeedOf(128).takesSeconds(15).build(), c -> c)
                 .oreGen(16, 0, 256, 10, WorldGen.NodeDimension.NETHER)
                 .recipe((prov, r) -> {
                     MIXING.add("cinder_flour_from_ore_pieces", b -> {
