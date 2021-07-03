d582e3f4508a3293bf3be02cdec78e7906bd9f4f
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-07-03 10:44:01 +0200

    i have no idea how i accidentally removed this method

--- src/main/java/com/kotakotik/createaddontemplate/register/recipes/ModMixingRecipes.java
+++ src/main/java/com/kotakotik/createaddontemplate/register/recipes/ModMixingRecipes.java
@@ -1,8 +1,12 @@
 package com.kotakotik.createaddontemplate.register.recipes;
 
 import com.simibubi.create.AllRecipeTypes;
+import com.simibubi.create.content.contraptions.processing.ProcessingRecipe;
+import com.simibubi.create.content.contraptions.processing.ProcessingRecipeBuilder;
 import com.simibubi.create.foundation.data.recipe.ProcessingRecipeGen;
 import net.minecraft.data.DataGenerator;
+
+import java.util.function.UnaryOperator;
 
 public class ModMixingRecipes extends ProcessingRecipeGen {
     public ModMixingRecipes(DataGenerator p_i48262_1_) {
@@ -14,4 +18,7 @@ public class ModMixingRecipes extends ProcessingRecipeGen {
         return AllRecipeTypes.MIXING;
     }
 
+    public <T extends ProcessingRecipe<?>> void add(String name, UnaryOperator<ProcessingRecipeBuilder<T>> transform) {
+        super.create(name, transform);
+    }
 }
