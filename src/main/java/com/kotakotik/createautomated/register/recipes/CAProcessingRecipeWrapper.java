f185dd1d4f6189142307eb8db3be1f8ff1b15c64
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-07-11 01:37:03 +0200

    no longer need this thanks to the datagen fix

--- src/main/java/com/kotakotik/createautomated/register/recipes/CAProcessingRecipeWrapper.java
+++ src/main/java/com/kotakotik/createautomated/register/recipes/CAProcessingRecipeWrapper.java
@@ -1,6 +1,5 @@
 package com.kotakotik.createautomated.register.recipes;
 
-import com.google.common.collect.Sets;
 import com.google.gson.JsonObject;
 import com.kotakotik.createautomated.CreateAutomated;
 import com.simibubi.create.content.contraptions.processing.ProcessingRecipe;
@@ -12,12 +11,10 @@ import net.minecraft.data.RecipeProvider;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.UnaryOperator;
 
@@ -42,27 +39,4 @@ public abstract class CAProcessingRecipeWrapper<T extends ProcessingRecipe<?>> e
 	}
 
 	public abstract ProcessingRecipeBuilder<T> createBuilder(ResourceLocation id);
-
-	@Override
-	public void act(DirectoryCache p_200398_1_) {
-		// ugh
-		Path path = this.generator.getOutputFolder();
-		Set<ResourceLocation> set = Sets.newHashSet();
-		registerRecipes((p_200410_3_) -> {
-			if (!set.add(p_200410_3_.getID())) {
-				throw new IllegalStateException("Duplicate recipe " + p_200410_3_.getID());
-			} else {
-				try {
-					saveRecipeMethod.invoke(this, p_200398_1_, p_200410_3_.getRecipeJson(), path.resolve("data/" + CreateAutomated.modid + "/recipes/" + p_200410_3_.getID().getPath() + ".json"));
-				} catch (IllegalAccessException | InvocationTargetException e) {
-					e.printStackTrace();
-				}
-				JsonObject jsonobject = p_200410_3_.getAdvancementJson();
-				if (jsonobject != null) {
-					saveRecipeAdvancement(p_200398_1_, jsonobject, path.resolve("data/" + CreateAutomated.modid + "/advancements/" + p_200410_3_.getAdvancementID().getPath() + ".json"));
-				}
-
-			}
-		});
-	}
 }
