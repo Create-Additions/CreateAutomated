c6f7f8fa3003c75022863daf516bea10e3816757
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-07-20 22:26:02 +0300

    :weary: hopefully fix fan crash fix mixin crashing in non dev environment

--- src/main/java/com/kotakotik/createautomated/mixin/temporary/FanCrashFixMixin.java
+++ src/main/java/com/kotakotik/createautomated/mixin/temporary/FanCrashFixMixin.java
@@ -5,8 +5,10 @@ import net.minecraft.client.audio.TickableSound;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AirCurrentSound.class)
 @Pseudo
@@ -15,11 +17,9 @@ public abstract class FanCrashFixMixin extends TickableSound {
 		super(p_i46532_1_, p_i46532_2_);
 	}
 
-	/**
-	 * @author kotakotik22
-	 */
-	@Overwrite(remap = false)
-	public void stop() {
+	@Inject(method = "stop", at = @At("HEAD"), cancellable = true)
+	public void stop(CallbackInfo ci) {
 		super.stop();
+		ci.cancel();
 	}
 }
