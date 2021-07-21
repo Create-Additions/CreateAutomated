bc2dea62a41e6662f2e89b9224a2e65016d48fa7
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-07-21 16:56:50 +0300

    fix  extractor drill clipping when powered by redstone

--- src/main/java/com/kotakotik/createautomated/content/processing/oreExtractor/OreExtractorInstance.java
+++ src/main/java/com/kotakotik/createautomated/content/processing/oreExtractor/OreExtractorInstance.java
@@ -2,25 +2,37 @@ package com.kotakotik.createautomated.content.processing.oreExtractor;
 
 import com.jozufozu.flywheel.backend.instancing.IDynamicInstance;
 import com.jozufozu.flywheel.backend.instancing.InstanceData;
+import com.jozufozu.flywheel.backend.instancing.Instancer;
 import com.jozufozu.flywheel.backend.instancing.MaterialManager;
 import com.kotakotik.createautomated.api.DrillPartialIndex;
-import com.kotakotik.createautomated.content.base.instances.CogInstance;
+import com.kotakotik.createautomated.register.ModBlockPartials;
 import com.simibubi.create.content.contraptions.base.KineticData;
-import com.simibubi.create.content.contraptions.base.KineticTileEntity;
+import com.simibubi.create.content.contraptions.base.KineticTileInstance;
 import com.simibubi.create.content.contraptions.base.RotatingData;
+import com.simibubi.create.foundation.render.AllMaterialSpecs;
+import net.minecraft.block.BlockState;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
 import java.lang.reflect.Field;
 
-public class OreExtractorInstance extends CogInstance implements IDynamicInstance {
+public class OreExtractorInstance extends KineticTileInstance<OreExtractorTile> implements IDynamicInstance {
 	public static final Field isRemovedField = ObfuscationReflectionHelper.findField(InstanceData.class, "removed");
 	public static final Field rotationOffsetField = ObfuscationReflectionHelper.findField(KineticData.class, "rotationOffset");
 
 	public RotatingData drill;
 
-	public OreExtractorInstance(MaterialManager<?> modelManager, KineticTileEntity tile) {
+	public Instancer<RotatingData> shortModel = materialManager.getMaterial(AllMaterialSpecs.ROTATING).getModel(ModBlockPartials.HALF_SHAFT_COGWHEEL, tile.getBlockState());
+	public Instancer<RotatingData> model = materialManager.getMaterial(AllMaterialSpecs.ROTATING).getModel(ModBlockPartials.COGWHEEL, tile.getBlockState());
+
+	protected RotatingData rotatingModel = this.setup(model.createInstance());
+
+	protected BlockState getRenderedBlockState() {
+		return this.blockState;
+	}
+
+	public OreExtractorInstance(MaterialManager<?> modelManager, OreExtractorTile tile) {
 		super(modelManager, tile);
 
 		if (getTile().durability > 0) {
@@ -37,6 +49,9 @@ public class OreExtractorInstance extends CogInstance implements IDynamicInstanc
 		drill.setRotationAxis(Direction.Axis.Y);
 		updateDrillRotation();
 		drill.setPosition(getInstancePosition().below());
+		rotatingModel.delete();
+		rotatingModel = createShortCogwheel();
+		rotatingModel.setPosition(getInstancePosition());
 		return drill;
 	}
 
@@ -48,6 +63,22 @@ public class OreExtractorInstance extends CogInstance implements IDynamicInstanc
 			e.printStackTrace();
 		}
 		drill.setRotationAxis(super.getRotationAxis());
+	}
+
+	public void deleteDrill() {
+		drill.delete();
+		rotatingModel.delete();
+		rotatingModel = createCogwheel();
+		rotatingModel.setPosition(getInstancePosition());
+		updateLight();
+	}
+
+	public RotatingData createShortCogwheel() {
+		return shortModel.createInstance();
+	}
+
+	public RotatingData createCogwheel() {
+		return model.createInstance();
 	}
 
 	@Override
@@ -69,7 +100,7 @@ public class OreExtractorInstance extends CogInstance implements IDynamicInstanc
 	public void update() {
 		if (getTile().durability <= 0) {
 			if (!isDrillRemoved()) {
-				drill.delete();
+				deleteDrill();
 			}
 		} else {
 			if (isDrillRemoved()) {
@@ -82,6 +113,7 @@ public class OreExtractorInstance extends CogInstance implements IDynamicInstanc
 			BlockPos p = getInstancePosition();
 			drill.setPosition(p.getX(), p.getY() - 1 + getTile().drillPos, p.getZ());
 		}
+		this.updateRotation(this.rotatingModel);
 		super.update();
 	}
 
@@ -91,11 +123,12 @@ public class OreExtractorInstance extends CogInstance implements IDynamicInstanc
 		if (!isDrillRemoved()) {
 			relight(this.pos.below(), drill);
 		}
+		relight(this.pos, this.rotatingModel);
 	}
 
 	@Override
 	public void remove() {
-		super.remove();
+		this.rotatingModel.delete();
 		if (drill != null) drill.delete();
 	}
 }
