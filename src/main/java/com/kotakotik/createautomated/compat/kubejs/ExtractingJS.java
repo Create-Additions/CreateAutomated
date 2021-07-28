503c43c857b72d1bd9427329aca03b1c1a76d5c4
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-07-28 12:38:50 +0300

    i think kubejs compat is working
    IDK??? THIS IS SO CONFUSING???

--- src/main/java/com/kotakotik/createautomated/compat/kubejs/ExtractingJS.java
+++ src/main/java/com/kotakotik/createautomated/compat/kubejs/ExtractingJS.java
@@ -1,38 +1,32 @@
 package com.kotakotik.createautomated.compat.kubejs;
 
-import com.blamejared.crafttweaker.api.item.IIngredient;
-import dev.latvian.kubejs.item.ItemJS;
-import dev.latvian.kubejs.item.ItemStackJS;
 import dev.latvian.kubejs.item.ingredient.IngredientJS;
-import dev.latvian.kubejs.recipe.RecipeExceptionJS;
 import dev.latvian.kubejs.recipe.RecipeJS;
 import dev.latvian.kubejs.util.ListJS;
 
 public class ExtractingJS extends RecipeJS {
+	// i uh
+	// i dont understand anything
 	public String output;
 	public IngredientJS node;
-	public int drillDamage;
-	public int requiredProgress;
-	public int minOre;
-	public int maxOre;
+//	public int drillDamage;
+//	public int requiredProgress;
+//	public int minOre;
+//	public int maxOre;
 
 	@Override
 	public void create(ListJS args) {
-		json.add("node", ((IngredientJS) args.get(0)).toJson());
-		Object output = args.get(1);
-		String outputOut;
-		if(output instanceof String) outputOut = (String) output;
-		else if(output instanceof ItemJS) outputOut = ((ItemJS) output).getRegistryName().toString();
-		else if(output instanceof ItemStackJS) outputOut = ((ItemStackJS) output).getItem().getRegistryName().toString();
-		else {
-			throw new RecipeExceptionJS("Recieved extracting output isn't a string, item, or item stack\n" + output + "\nReceived class: " + output.getClass().getName());
-		}
-		json.addProperty("output", outputOut);
+		node = parseIngredientItem(args.get(0));
+		output = parseResultItem(args.get(1)).getId();
 
 		// defaults
 		drillDamage(1);
 		requiredProgressSeconds(128, 40);
 		ore(1, 4);
+//		drillDamage = 1;
+//		requiredProgress = 128 * 40;
+//		minOre = 1;
+//		maxOre = 4;
 	}
 
 	public ExtractingJS drillDamage(int damage) {
@@ -89,19 +83,19 @@ public class ExtractingJS extends RecipeJS {
 	public void deserialize() {
 		output = json.get("output").getAsString();
 		node = parseResultItem(json.get("node"));
-		drillDamage = json.get("drillDamage").getAsInt();
-		requiredProgress = json.get("requiredProgress").getAsInt();
-		minOre = json.get("minOre").getAsInt();
-		maxOre = json.get("maxOre").getAsInt();
+//		drillDamage = json.get("drillDamage").getAsInt();
+//		requiredProgress = json.get("requiredProgress").getAsInt();
+//		minOre = json.get("minOre").getAsInt();
+//		maxOre = json.get("maxOre").getAsInt();
 	}
 
 	@Override
 	public void serialize() {
 		json.addProperty("output", output);
 		json.add("node", node.toJson());
-		json.addProperty("drillDamage", drillDamage);
-		json.addProperty("requiredProgress", requiredProgress);
-		json.addProperty("minOre", minOre);
-		json.addProperty("maxOre", maxOre);
+//		json.addProperty("drillDamage", drillDamage);
+//		json.addProperty("requiredProgress", requiredProgress);
+//		json.addProperty("minOre", minOre);
+//		json.addProperty("maxOre", maxOre);
 	}
 }
