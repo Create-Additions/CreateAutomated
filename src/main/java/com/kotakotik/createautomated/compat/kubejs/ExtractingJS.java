bce19830a91b542eebce0f576fb8b42954077a7f
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-07-28 15:55:20 +0300

    use inputItems in extracting js
    hehe padding my commits

--- src/main/java/com/kotakotik/createautomated/compat/kubejs/ExtractingJS.java
+++ src/main/java/com/kotakotik/createautomated/compat/kubejs/ExtractingJS.java
@@ -1,22 +1,20 @@
 package com.kotakotik.createautomated.compat.kubejs;
 
-import dev.latvian.kubejs.item.ingredient.IngredientJS;
 import dev.latvian.kubejs.recipe.RecipeJS;
 import dev.latvian.kubejs.util.ListJS;
 
 public class ExtractingJS extends RecipeJS {
 	// i uh
 	// i dont understand anything
-	public String output;
-	public IngredientJS node;
 //	public int drillDamage;
 //	public int requiredProgress;
 //	public int minOre;
 //	public int maxOre;
+	public String output;
 
 	@Override
 	public void create(ListJS args) {
-		node = parseIngredientItem(args.get(0));
+		inputItems.add(parseIngredientItem(args.get(0)));
 		output = parseResultItem(args.get(1)).getId();
 
 		// defaults
@@ -82,7 +80,7 @@ public class ExtractingJS extends RecipeJS {
 	@Override
 	public void deserialize() {
 		output = json.get("output").getAsString();
-		node = parseResultItem(json.get("node"));
+		inputItems.add(parseResultItem(json.get("node")));
 //		drillDamage = json.get("drillDamage").getAsInt();
 //		requiredProgress = json.get("requiredProgress").getAsInt();
 //		minOre = json.get("minOre").getAsInt();
@@ -92,7 +90,7 @@ public class ExtractingJS extends RecipeJS {
 	@Override
 	public void serialize() {
 		json.addProperty("output", output);
-		json.add("node", node.toJson());
+		json.add("node", inputItems.get(0).toJson());
 //		json.addProperty("drillDamage", drillDamage);
 //		json.addProperty("requiredProgress", requiredProgress);
 //		json.addProperty("minOre", minOre);
