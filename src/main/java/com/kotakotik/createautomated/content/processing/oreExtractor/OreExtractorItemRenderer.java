0236b00429e46c4f2f45d4b499fa45795a8ca536
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-07-31 17:43:04 +0300

    fix extractor item offset

--- src/main/java/com/kotakotik/createautomated/content/processing/oreExtractor/OreExtractorItemRenderer.java
+++ src/main/java/com/kotakotik/createautomated/content/processing/oreExtractor/OreExtractorItemRenderer.java
@@ -26,17 +26,21 @@ public class OreExtractorItemRenderer extends ItemStackTileEntityRenderer {
 
 	public BlockState state = ModBlocks.ORE_EXTRACTOR_TOP.getDefaultState();
 
-	float offset = 1.2f;
+	float offset = 1.4f;
+	float inventoryOffset = -.3f;
 
 	@Override
-	public void renderByItem(ItemStack stack, ItemCameraTransforms.TransformType p_239207_2_, MatrixStack ms, IRenderTypeBuffer buffer, int p_239207_5_, int p_239207_6_) {
-		offset = 1.4f;
+	public void renderByItem(ItemStack stack, ItemCameraTransforms.TransformType type, MatrixStack ms, IRenderTypeBuffer buffer, int p_239207_5_, int p_239207_6_) {
+		float o = offset;
+		if (type == ItemCameraTransforms.TransformType.GUI) {
+			o += inventoryOffset;
+		}
 		IVertexBuilder vb = buffer.getBuffer(RenderType.cutout());
 		PartialBufferer.get(ModBlockPartials.ORE_EXTRACTOR_TOP, state)
-				.translate(0, offset, 0)
+				.translate(0, o, 0)
 				.renderInto(ms, vb);
 		PartialBufferer.get(ModBlockPartials.ORE_EXTRACTOR_BOTTOM, state)
-				.translate(0, -1 + offset, 0)
+				.translate(0, -1 + o, 0)
 				.renderInto(ms, vb);
 		PartialModel cogModel = ModBlockPartials.COGWHEEL;
 		if (!stack.isEmpty()) {
@@ -46,14 +50,14 @@ public class OreExtractorItemRenderer extends ItemStackTileEntityRenderer {
 				if (tileData.contains("DrillId") && tileData.getInt("Durability") != 0) {
 					ResourceLocation drillId = new ResourceLocation(tileData.getString("DrillId"));
 					SuperByteBuffer headRender = PartialBufferer.get(DrillPartialIndex.get(drillId), state);
-					headRender.translate(0, -1 + offset, 0).renderInto(ms, vb);
+					headRender.translate(0, -1 + o, 0).renderInto(ms, vb);
 					cogModel = ModBlockPartials.HALF_SHAFT_COGWHEEL;
 				}
 			}
 		}
 		PartialBufferer.get(cogModel, state)
-				.translate(0, offset, 0)
+				.translate(0, o, 0)
 				.renderInto(ms, vb);
-		super.renderByItem(stack, p_239207_2_, ms, buffer, p_239207_5_, p_239207_6_);
+		super.renderByItem(stack, type, ms, buffer, p_239207_5_, p_239207_6_);
 	}
 }
