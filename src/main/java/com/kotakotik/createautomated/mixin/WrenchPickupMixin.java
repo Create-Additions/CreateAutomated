107fa859167afc2c9faadd3096d1f2b2473923a3
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-08-01 12:00:50 +0300

    fix #3

--- src/main/java/com/kotakotik/createautomated/mixin/WrenchPickupMixin.java
+++ src/main/java/com/kotakotik/createautomated/mixin/WrenchPickupMixin.java
@@ -1,11 +1,13 @@
-package com.kotakotik.createautomated.content.simple.node;
+package com.kotakotik.createautomated.mixin;
 
 import com.kotakotik.createautomated.CALocalization;
+import com.kotakotik.createautomated.content.simple.node.NodeBlock;
+import com.kotakotik.createautomated.register.config.ModConfig;
 import com.simibubi.create.AllItems;
 import com.simibubi.create.content.contraptions.components.structureMovement.AbstractContraptionEntity;
 import com.simibubi.create.content.contraptions.components.structureMovement.Contraption;
 import com.simibubi.create.content.contraptions.components.structureMovement.OrientedContraptionEntity;
-import com.simibubi.create.foundation.config.AllConfigs;
+import com.simibubi.create.content.contraptions.components.structureMovement.mounted.MinecartContraptionItem;
 import com.simibubi.create.foundation.config.CKinetics;
 import com.simibubi.create.foundation.utility.Lang;
 import net.minecraft.entity.Entity;
@@ -14,49 +16,52 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 
-@Mod.EventBusSubscriber
-public class WrenchPickupEvent {
-	@SubscribeEvent(priority = EventPriority.HIGH)
-	public static void event(PlayerInteractEvent.EntityInteract event) {
-		if (AllConfigs.SERVER.kinetics.spawnerMovement.get() != CKinetics.SpawnerMovementSetting.NO_PICKUP) return;
+// mixin abuse? 😩
+@Mixin(MinecartContraptionItem.class)
+public class WrenchPickupMixin {
+	@Inject(method = "wrenchCanBeUsedToPickUpMinecartContraptions", at = @At("HEAD"), cancellable = true, remap = false)
+	private static void event(PlayerInteractEvent.EntityInteract event, CallbackInfo ci) {
+		if (ModConfig.SERVER.machines.extractor.nodeMovement.get() != CKinetics.SpawnerMovementSetting.NO_PICKUP)
+			return;
 		// pain?
 		Entity entity = event.getTarget();
 		PlayerEntity player = event.getPlayer();
 		if (player == null || entity == null) {
-			event.setCanceled(true);
+			ci.cancel();
 			return;
 		}
 
 		ItemStack wrench = player.getItemInHand(event.getHand());
 		if (!AllItems.WRENCH.isIn(wrench)) {
-			event.setCanceled(true);
+			ci.cancel();
 			return;
 		}
 		if (entity instanceof AbstractContraptionEntity)
 			entity = entity.getVehicle();
 		if (!(entity instanceof AbstractMinecartEntity)) {
-			event.setCanceled(true);
+			ci.cancel();
 			return;
 		}
 		if (!entity.isAlive()) {
-			event.setCanceled(true);
+			ci.cancel();
 			return;
 		}
 		AbstractMinecartEntity cart = (AbstractMinecartEntity) entity;
 		AbstractMinecartEntity.Type type = cart.getMinecartType();
 		if (type != AbstractMinecartEntity.Type.RIDEABLE && type != AbstractMinecartEntity.Type.FURNACE && type != AbstractMinecartEntity.Type.CHEST) {
-			event.setCanceled(true);
+			ci.cancel();
 			return;
 		}
 		List<Entity> passengers = cart.getPassengers();
 		if (passengers.isEmpty() || !(passengers.get(0) instanceof OrientedContraptionEntity)) {
-			event.setCanceled(true);
+			ci.cancel();
 			return;
 		}
 		OrientedContraptionEntity contraption = (OrientedContraptionEntity) passengers.get(0);
@@ -66,7 +71,7 @@ public class WrenchPickupEvent {
 				.anyMatch(i -> i.state.getBlock() instanceof NodeBlock)) {
 			player.displayClientMessage(Lang.translate(CALocalization.noPickupNodeKey)
 					.withStyle(TextFormatting.RED), true);
-			event.setCanceled(true);
+			ci.cancel();
 		}
 	}
 }
