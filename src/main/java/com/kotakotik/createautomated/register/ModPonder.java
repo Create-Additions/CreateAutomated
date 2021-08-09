eacbffbdd0efa669cceba115ecb578253b317973
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-08-09 10:29:32 +0300

    update to create 0.3.2d

--- src/main/java/com/kotakotik/createautomated/register/ModPonder.java
+++ src/main/java/com/kotakotik/createautomated/register/ModPonder.java
@@ -4,6 +4,7 @@ import com.kotakotik.createautomated.CreateAutomated;
 import com.kotakotik.createautomated.content.ponder.OreExtractorScenes;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.simibubi.create.foundation.ponder.PonderLocalization;
+import com.simibubi.create.foundation.ponder.PonderRegistrationHelper;
 import com.simibubi.create.foundation.ponder.PonderRegistry;
 import com.simibubi.create.foundation.ponder.content.PonderTag;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -15,14 +16,13 @@ public class ModPonder {
 	}
 
 	protected static void register() {
-		PonderRegistry.startRegistration(CreateAutomated.MODID);
-		PonderRegistry.forComponents(ModBlocks.ORE_EXTRACTOR_TOP)
+		new PonderRegistrationHelper(CreateAutomated.MODID)
+				.forComponents(ModBlocks.ORE_EXTRACTOR_TOP)
 				.addStoryBoard("extractor/intro", OreExtractorScenes::intro)
 				.addStoryBoard("extractor/automation", OreExtractorScenes::automation);
 
 		PonderRegistry.TAGS.forTag(PonderTag.KINETIC_APPLIANCES)
 				.add(ModBlocks.ORE_EXTRACTOR_TOP);
-		PonderRegistry.endRegistration();
 	}
 
 	public static void generateLang(CreateRegistrate registrate, GatherDataEvent event) {
