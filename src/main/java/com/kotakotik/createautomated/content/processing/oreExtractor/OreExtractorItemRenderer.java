b755d130084b5b0328a3529a53ae13eb2e5b414d
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-08-09 11:16:27 +0300

    fix extractor item lighting
    i have no idea what im doing

--- src/main/java/com/kotakotik/createautomated/content/processing/oreExtractor/OreExtractorItemRenderer.java
+++ src/main/java/com/kotakotik/createautomated/content/processing/oreExtractor/OreExtractorItemRenderer.java
@@ -31,16 +31,18 @@ public class OreExtractorItemRenderer extends ItemStackTileEntityRenderer {
 	public float inventoryOffset = -.3f;
 
 	@Override
-	public void renderByItem(ItemStack stack, ItemCameraTransforms.TransformType type, MatrixStack ms, IRenderTypeBuffer buffer, int p_239207_5_, int p_239207_6_) {
+	public void renderByItem(ItemStack stack, ItemCameraTransforms.TransformType type, MatrixStack ms, IRenderTypeBuffer buffer, int light, int p_239207_6_) {
 		float o = offset;
 		if (type == ItemCameraTransforms.TransformType.GUI) {
 			o += inventoryOffset;
 		}
 		IVertexBuilder vb = buffer.getBuffer(RenderType.cutout());
 		PartialBufferer.get(ModBlockPartials.ORE_EXTRACTOR_TOP, state)
+				.light(light)
 				.translate(0, o, 0)
 				.renderInto(ms, vb);
 		PartialBufferer.get(ModBlockPartials.ORE_EXTRACTOR_BOTTOM, state)
+				.light(light)
 				.translate(0, -1 + o, 0)
 				.renderInto(ms, vb);
 		PartialModel cogModel = ModBlockPartials.COGWHEEL;
@@ -51,14 +53,15 @@ public class OreExtractorItemRenderer extends ItemStackTileEntityRenderer {
 				if (tileData.contains("DrillId") && tileData.getInt("Durability") != 0) {
 					ResourceLocation drillId = new ResourceLocation(tileData.getString("DrillId"));
 					SuperByteBuffer headRender = PartialBufferer.get(DrillPartialIndex.get(drillId), state);
-					headRender.translate(0, -1 + o, 0).renderInto(ms, vb);
+					headRender.translate(0, -1 + o, 0).light(0).renderInto(ms, vb);
 					cogModel = ModBlockPartials.HALF_SHAFT_COGWHEEL;
 				}
 			}
 		}
 		PartialBufferer.get(cogModel, state)
+				.light(light)
 				.translate(0, o, 0)
 				.renderInto(ms, vb);
-		super.renderByItem(stack, type, ms, buffer, p_239207_5_, p_239207_6_);
+		super.renderByItem(stack, type, ms, buffer, light, p_239207_6_);
 	}
 }
